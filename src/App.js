b9a070c00eb69dfb36841511116ed869532455d9
constance <acoderbeauty@163.com>
2023-06-25 14:59:59 +0800

    feat: marquee

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Marquee, { Wrapper } from './marquee.js'
+
+const DATA_LIST = [
+  "Marquee Text Test 1",
+  "Marquee Text Test 2",
+  "Marquee Text Test 3"
+];
+
+
+const DATA_LIST2 = ["A", "B", "C"];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Marquee list={DATA_LIST} time={5} />
+      <Marquee list={DATA_LIST2} time={5} />
+      <Marquee list={DATA_LIST} time={2.5} toRight />
+    </Wrapper>
   );
 }
 
